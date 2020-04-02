7232851f6d5615967e1b6e82bccc4923c68e7295
arily <lbsaaaaa@gmail.com>
2020-04-02 12:00:05 +0900

    split config from builder

---
+++ config/koishi.js
@@ -0,0 +1,12 @@
+module.exports = {
+    type: 'ws',
+    port: 7070,
+    server: 'http://10.80.0.2:6700',
+    name: '小阿日',
+    commandPrefix: ["!", "！"],
+    logFilter: {
+    	CabbageReaction: 3,
+    }
+    // selfId: 1840567854
+    // 在这里加上其他配置信息
+}
