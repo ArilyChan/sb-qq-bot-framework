63f7856d33b4b53f837cc35788cd04403b7014da
arily <lbsaaaaa@gmail.com>
2020-08-30 09:49:15 +0900

    filters

--- config/contextPlugins.js
+++ config/contextPlugins.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const appDir = path.dirname(require.main.filename)
 
+// const filters = require('../lib/filters')
+
 const ContextBuilder = require(`${appDir}/lib/contextBuilder`)
 // const PluginLoader = require(`${appDir}/lib/Loader`)
 // const Wrapper = require(`${appDir}/lib/Wrapper`)
@@ -16,11 +18,11 @@ module.exports = [
       type: 'local',
       path: 'Plugins/arily/BlackFarts',
       priority: 1,
-      filter (meta) {
-        if (meta.userId === 1145141919) return false
-
-        return true
-      }
+      filter: [(meta) => {
+        const block = meta.$parsed === '吃啥' && meta.groupId === 263668213
+        if (block) meta.$send('去别的群试试吧.')
+        return block
+      }]
     }]
   }
 ]
