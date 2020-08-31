017699f5c5c3369ff46f1f8f1c9f185190f4cc86
arily <lbsaaaaa@gmail.com>
2020-08-31 18:30:10 +0900

    Update contextPlugins.js

--- config/contextPlugins.js
+++ config/contextPlugins.js
@@ -22,6 +22,16 @@ module.exports = [
         meta => filters.blockGroup(263668213)(meta).then(result => !(!result && ['吃啥', '吃什么', '吃什麼'].includes(meta.$parsed.message) && meta.$parsed.prefix !== null)).then(result => {
           if (!result) meta.$send('去别的群试试吧.')
           return result
+        }),
+        meta => filters.blockGroup(738401694)(meta).then(result => !(!result && ['吃啥', '吃什么', '吃什麼'].includes(meta.$parsed.message) && meta.$parsed.prefix !== null)).then(result => {
+          if (result) return true
+          const now = new Date()
+          const hour = now.getHours()
+          if ((hour >= 2 && hour <= 5) || (hour >= 10 && hour <= 11) || (hour >= 14 && hour <= 17)) {
+            meta.$send('爆炸不让吃...要不要去菜品群吃? 加%㪊 1097526643')
+            return false
+          }
+          return true
         })
       ]
     }]
