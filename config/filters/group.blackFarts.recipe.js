a57baaa2dae61650a67eecb9ff71af26d8c29fd4
arily <lbsaaaaa@gmail.com>
2020-09-02 11:46:46 +0900

    remove unnecessary file

--- config/filters/group.blackFarts.recipe.js
+++
@@ -1,2 +0,0 @@
-const filters = require('../../lib/filters')
-module.exports = (...groups) => meta => filters.blockGroup(...groups)(meta).then(result => !(!result && ['吃啥', '吃什么', '吃什麼'].includes(meta.$parsed.message) && meta.$parsed.prefix !== null))
